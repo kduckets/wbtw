267fa274b334c96c77a4acfea693757c84a3415e
kduckets <kmditroia@gmail.com>
2020-11-25 20:28:32 -0500

    blink 3s

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "93be17c1b2488753a5a6";
+/******/ 	var hotCurrentHash = "00cb248ac36e643e602d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
