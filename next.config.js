66239c026f46857db4d129fc634c70dc36feff94
kduckets <kmditroia@gmail.com>
2021-09-16 20:39:08 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-module.exports = {
-    webpack(config, options) {
-      config.module.rules.push({
-        test: /\.mp3$/,
-        use: {
-          loader: 'url-loader',
-        },
-      });
-      return config;
-    },
-  }
+// module.exports = {
+//     webpack(config, options) {
+//       config.module.rules.push({
+//         test: /\.mp3$/,
+//         use: {
+//           loader: 'url-loader',
+//         },
+//       });
+//       return config;
+//     },
+//   }
